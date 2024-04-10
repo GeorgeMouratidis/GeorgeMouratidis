70ed13b36bbf1d9704a10ade969187ef7b1ddc8f
GeorgeMouratidis <105636056+GeorgeMouratidis@users.noreply.github.com>
2024-04-10 19:59:42 +0300

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,5 +1,18 @@
+function goto(id){
+    window.scrollTo(0,scrollY+document.querySelector("#"+id).getBoundingClientRect().top-200);
+}
+
 
 window.scrollTo(0, 0);
+
+document.getElementById("gotoproj").addEventListener("click",(e)=>{
+    goto("proj");
+
+})
+
+
+
+
 
 document.querySelector(".education").scrollTop=28.79;
 ;
